f42bc26ab14a5117db792ad1fbfce98d12a0e5fc
bdsl-opp <dlfgofk09@gmail.com>
2026-05-15 15:57:23 +0200

    Update index.js

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,14 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    let path = url.pathname;
-    if (path === "/" || path === "") {
-      path = "/coming-soon.html";
-    }
-    try {
-      return await env.ASSETS.fetch(new Request(new URL(path, request.url)));
-    } catch (e) {
-      return new Response("Not Found: " + path, { status: 404 });
-    }
+    return env.ASSETS.fetch(request);
   }
 };
